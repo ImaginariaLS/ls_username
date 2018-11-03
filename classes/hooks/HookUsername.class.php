84fbabde902bb42d4c2ecf45e3e0d09b75a625cf
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-03 23:54:43 +0300

    original

---
+++ classes/hooks/HookUsername.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class PluginUsername_HookUsername extends Hook {
+
+    public function RegisterHook() {
+        $this->AddHook('template_html_head_end','UserName');
+    }
+    
+    public function UserName() {
+        $sSelector = 'body *';
+        if(Config::Get('plugin.username.article')) {
+            $sSelector = '.topic-content';
+        }
+        if($this->User_IsAuthorization()){
+            $this->Viewer_Assign('sSelector', $sSelector);
+            return $this->Viewer_Fetch(Plugin::GetTemplatePath(__CLASS__).'username.tpl');
+        }
+    }
+}
+?>
